a2c14324523aecfa2d4fa8ab52d4fc912e3a0a8c
Donny <dongreenberg2@gmail.com>
2022-07-28 17:54:21 -0400

    Add Send object stub

--- runhouse/rns/send.py
+++ runhouse/rns/send.py
@@ -1,11 +1,26 @@
+from typing import Optional, Callable, Dict, Union
+
 import ray
 
+class Send:
 
-def send(fn):
-    ray.init(local_mode=True)
-    remote_fn = ray.remote(fn)
+    def __init__(self,
+                 name=None,):
+        self.name = name
+        # Check kv store if send exists
+        ray.init(local_mode=True)
+        pass
 
-    def sent_fn(*args):
-        res = remote_fn.remote(*args)
+
+def send(fn: Callable,
+         hardware: Union[None, str, Dict[str, int]] = None,
+         runtime_env: Union[None, Dict] = None
+         ):
+    # Resolve hardware properly through rh API (like just pull down the resources info to pass to ray)
+    remote_fn = ray.remote(resources=hardware,
+                           runtime_env=runtime_env)(fn)
+
+    def sent_fn(*args, **kwargs):
+        res = remote_fn.remote(*args, **kwargs)
         return ray.get(res)
     return sent_fn
--- tests/test_send.py
+++ tests/test_send.py
@@ -5,10 +5,11 @@ import runhouse as rh
 class TestSend(unittest.TestCase):
 
     def test_create_send(self):
-        sum = lambda a, b: a + b
-        remote_sum = rh.send(sum)
+        def summer(a, b):
+            return a + b
+        remote_sum = rh.send(summer)
         res = remote_sum(1, 5)
-        self.assertEqual(res, 6)  # add assertion here
+        self.assertEqual(res, 6)
 
 
 if __name__ == '__main__':
