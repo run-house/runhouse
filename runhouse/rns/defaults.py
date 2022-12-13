f9ac9e24371094650f6d580616d07843e7d6cc0d
Donny <dongreenberg2@gmail.com>
2022-12-12 20:28:50 -0500

    Fix default issue.

--- runhouse/rns/defaults.py
+++ runhouse/rns/defaults.py
@@ -96,12 +96,10 @@ class Defaults:
             raise Exception(f'Failed to download defaults for {entity}, received status code {resp.status_code}')
         resp_data: dict = read_response_data(resp)
         raw_defaults = resp_data.get('config', {})
-        raw_defaults["username"] = resp_data.get('username')
-        raw_defaults["token"] = headers.get('Authorization')[7:]
-        # TODO [DG] fix this
-        # raw_defaults['default_folder'] = raw_defaults["username"] if \
-        #     raw_defaults['default_folder'] == 'default/' else raw_defaults['default_folder']
-        raw_defaults['default_folder'] = raw_defaults.get("username", '/default')
+        raw_defaults['username'] = resp_data.get('username')
+        raw_defaults['token'] = headers.get('Authorization')[7:]
+        raw_defaults['default_folder'] = raw_defaults['username'] if \
+            raw_defaults.get('default_folder') == '/default' else raw_defaults.get('default_folder')
         formatted = {k: to_bool(v) for k, v in raw_defaults.items()}
 
         self.defaults_cache = copy.deepcopy(formatted)
