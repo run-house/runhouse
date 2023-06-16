67bc73567ae2b5e33f2db766e1a1d7d1ae55c383
Caroline <chen.caroline12@gmail.com>
2023-06-15 20:45:53 -0400

    logout flow for custom providers

--- runhouse/rns/login.py
+++ runhouse/rns/login.py
@@ -141,24 +141,34 @@ def logout(
     interactive_session: bool = (
         interactive if interactive is not None else is_interactive()
     )
-    for (provider_name, secret) in configs.get("secrets", {}).items():
-        provider = Secrets.builtin_provider_class_from_name(provider_name)
+
+    for (provider_name, _) in configs.get("secrets", {}).items():
         if provider_name == "ssh":
             continue
-        provider_creds_path: Union[str, tuple] = provider.default_credentials_path()
+
+        provider = Secrets.builtin_provider_class_from_name(provider_name)
 
         if interactive_session:
             delete_loaded_secrets = typer.confirm(
                 f"Delete credentials file for {provider_name}?"
             )
 
-        configs.delete(provider_name)
+        if provider:
+            provider_creds_path: Union[str, tuple] = provider.default_credentials_path()
 
-        if delete_loaded_secrets:
-            provider.delete_secrets_file(provider_creds_path)
-            logger.info(
-                f"Deleted {provider_name} credentials file from path: {provider_creds_path}"
-            )
+            configs.delete(provider_name)
+
+            if delete_loaded_secrets:
+                provider.delete_secrets_file(provider_creds_path)
+                logger.info(
+                    f"Deleted {provider_name} credentials file from path: {provider_creds_path}"
+                )
+        else:
+            if delete_loaded_secrets:
+                logger.warning(
+                    f"Unable to delete credentials file for custom provider {provider_name}. "
+                    "Please delete the file manually."
+                )
 
     # Delete token from rh config file
     configs.delete(key="token")
