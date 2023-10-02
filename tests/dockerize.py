a1ef90d235cf905c679eb268b32185716a50cb45
Denis K <denis@run.house>
2023-10-02 12:59:12 -0400

    3 Dockerfiles for 3 different setup options:
    * /docker/base: "runhouse start --host 0.0.0.0" in a Docker container. Does not support cluster.run(), rsync or SSH out of the box.
    * /docker/httpserver: the Runhouse HTTPServer binary in a Docker container. Main use case is integration tests (HTTP over port 50052).
    * /docker/ssh: "runhouse start --host 0.0.0.0" in a Docker container with ssh support. cluster.run() is supported. Current proof-of-concept auth is by username & plaintext password.

--- tests/dockerize.py
+++ tests/dockerize.py
@@ -1,6 +1,6 @@
 # Prerequisites: Start a local Docker image with Runhouse installed using the following commands:
 #   $ docker build --pull --rm -f \"Dockerfile\" -t runhouse:start .
-#   $ docker run --rm --shm-size=3gb -it -p 50052:50052 -p 6379:6379 -p 52365:52365 runhouse:start
+#   $ docker run --rm --shm-size=3gb -it -p 50052:50052 -p 6379:6379 -p 52365:52365 -p 22:22 runhouse:start
 print("Starting Runhouse in a local Docker cluster")
 import runhouse as rh
 
@@ -14,7 +14,7 @@ print("Cluster created")
 c.up_if_not()
 print("Cluster is up: ", c.is_up())
 c.check_server()
-# c.run(["ls -l"]) # doesn't currently work.
+c.run(["pwd"])
 print("Putting key my_key with value 7")
 c.put("my_key", "7")
 print("Getting my_key, value is: " + c.get("my_key"))
