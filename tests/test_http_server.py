41189597d10956c503181cdbda1c3cc2a7364705
Denis K <denis@run.house>
2023-11-01 15:19:30 -0400

    Update the existing test to use the renamed local_docker_cluster_passwd

--- tests/test_http_server.py
+++ tests/test_http_server.py
@@ -5,8 +5,8 @@ import pytest
 
 @pytest.mark.dockertest
 @pytest.mark.httpservertest
-def test_cluster_is_up(local_docker_slim):
-    cluster = local_docker_slim
+def test_cluster_is_up(local_docker_cluster_passwd):
+    cluster = local_docker_cluster_passwd
     cluster.check_server()
     assert cluster.is_up()  # Should be true for a Cluster object
 
