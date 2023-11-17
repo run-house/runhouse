f6d6646893846f74113fbec1f26a3464d8ba659a
dongreenberg <dongreenberg2@gmail.com>
2023-11-17 12:29:09 -0500

    Style

--- tests/test_resources/test_modules/test_server_modules/assets/sample_fastapi_app.py
+++ tests/test_resources/test_modules/test_server_modules/assets/sample_fastapi_app.py
@@ -7,14 +7,17 @@ app = fastapi.FastAPI()
 def summer(a: int, b: int):
     return a + b
 
+
 @app.post("/my/deeply/{arg1}/nested/endpoint/{arg2}")
 async def my_deeply_nested_saync_endpoint(arg1: str, arg2: int, arg3: float):
     return arg1, arg2, arg3
+
 
 @app.get("/my/streaming/endpoint")
 def my_streaming_endpoint():
     for i in range(10):
         yield i
+
 
 @app.get("/my/endpoint/with/optional/body/params/and/header")
 def my_endpoint_with_optional_body_params_and_header(
@@ -24,7 +27,8 @@ def my_endpoint_with_optional_body_params_and_header(
 ):
     return a, b, c
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, port=8000)
 
+    uvicorn.run(app, port=8000)
--- tests/test_resources/test_modules/test_server_modules/dont_test_server_module.py
+++ tests/test_resources/test_modules/test_server_modules/dont_test_server_module.py
@@ -1,12 +1,14 @@
+import asyncio
 import logging
 import unittest
-import asyncio
 
 import pytest
 import requests
 import runhouse as rh
 
-from tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app import app
+from tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app import (
+    app,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +21,7 @@ https://github.com/florimondmanca/awesome-asgi
 TODO test with Den Auth and HTTPS enabled
 """
 
+
 def test_asgi_server(cluster):
     # If we detect that this is a FastAPI app / ASGIApp, we can automatically attach the functions to the  __getattr__
     # of the app object, so that we can call them directly from the app object. We can also deploy it with uvicorn.
@@ -27,22 +30,38 @@ def test_asgi_server(cluster):
     # or maybe app.routes[4].dependant.cache_key[0]
     # We can find the app name by extracting the decorator from the function source:
     # inspect.getsource(app.routes[4].endpoint)
-    fast_api_module = rh.server(app).to(cluster, env = ["pytest", "requests"], name="fast_api_module")
+    fast_api_module = rh.server(app).to(
+        cluster, env=["pytest", "requests"], name="fast_api_module"
+    )
     assert isinstance(fast_api_module, rh.ASGIApp)
     assert fast_api_module.summer(1, 2) == 3
     # Call an async method
-    assert asyncio.run(fast_api_module.my_deeply_nested_saync_endpoint("hello", 1, 2.0) == ("hello", 1, 2.0))
+    assert asyncio.run(
+        fast_api_module.my_deeply_nested_saync_endpoint("hello", 1, 2.0)
+        == ("hello", 1, 2.0)
+    )
 
     assert fast_api_module.my_streaming_endpoint() == list(range(10))
-    assert fast_api_module.my_endpoint_with_optional_body_params_and_header(a=1, b=2, c=3) == (1, 2, 3)
+    assert fast_api_module.my_endpoint_with_optional_body_params_and_header(
+        a=1, b=2, c=3
+    ) == (1, 2, 3)
 
-    res = requests.get("http://localhost:8000/fast_api_module/summer/1", params={"b": 2})
+    res = requests.get(
+        "http://localhost:8000/fast_api_module/summer/1", params={"b": 2}
+    )
     assert res.json() == 3
-    res = requests.post("http://localhost:8000/fast_api_module/my/deeply/hello/nested/endpoint/1", json={"arg3": 2.0})
+    res = requests.post(
+        "http://localhost:8000/fast_api_module/my/deeply/hello/nested/endpoint/1",
+        json={"arg3": 2.0},
+    )
     assert res.json() == ["hello", 1, 2.0]
     res = requests.get("http://localhost:8000/fast_api_module/my/streaming/endpoint")
     assert res.json() == list(range(10))
-    res = requests.get("http://localhost:8000/fast_api_module/my/endpoint/with/optional/body/params/and/header", json={"a": 1, "b": 2}, headers={"c": 3})
+    res = requests.get(
+        "http://localhost:8000/fast_api_module/my/endpoint/with/optional/body/params/and/header",
+        json={"a": 1, "b": 2},
+        headers={"c": 3},
+    )
     assert res.json() == [1, 2, 3]
 
     # Test UI page pulls properly
@@ -55,19 +74,33 @@ def test_asgi_server(cluster):
     with pytest.raises(requests.exceptions.ConnectionError):
         requests.get("http://localhost:8000/fast_api_module/summer/1", params={"b": 2})
 
-def test_python_script_start(cluster):
-    mod = tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
-    # Will start with python -m tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
-    server_module = rh.server(mod, port=8000).to(cluster, env = ["pytest", "requests"], name="fast_api_module")
-    assert isinstance(mod, rh.WebServer)
 
-    res = requests.get("http://localhost:8001/fast_api_module/summer/1", params={"b": 2})
+def test_python_script_start(cluster):
+    mod = (
+        tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
+    )
+    # Will start with python -m tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
+    server_module = rh.server(mod, port=8000).to(
+        cluster, env=["pytest", "requests"], name="fast_api_module"
+    )
+    assert isinstance(server_module, rh.WebServer)
+
+    res = requests.get(
+        "http://localhost:8001/fast_api_module/summer/1", params={"b": 2}
+    )
     assert res.json() == 3
-    res = requests.post("http://localhost:8001/fast_api_module/my/deeply/hello/nested/endpoint/1", json={"arg3": 2.0})
+    res = requests.post(
+        "http://localhost:8001/fast_api_module/my/deeply/hello/nested/endpoint/1",
+        json={"arg3": 2.0},
+    )
     assert res.json() == ["hello", 1, 2.0]
     res = requests.get("http://localhost:8001/fast_api_module/my/streaming/endpoint")
     assert res.json() == list(range(10))
-    res = requests.get("http://localhost:8001/fast_api_module/my/endpoint/with/optional/body/params/and/header", json={"a": 1, "b": 2}, headers={"c": 3})
+    res = requests.get(
+        "http://localhost:8001/fast_api_module/my/endpoint/with/optional/body/params/and/header",
+        json={"a": 1, "b": 2},
+        headers={"c": 3},
+    )
     assert res.json() == [1, 2, 3]
 
     # Test UI page pulls properly
@@ -80,20 +113,36 @@ def test_python_script_start(cluster):
     with pytest.raises(requests.exceptions.ConnectionError):
         requests.get("http://localhost:8001/fast_api_module/summer/1", params={"b": 2})
 
+    server_module.stop()
+
+
 def test_custom_start_cmd(cluster):
-    mod = tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
     # Will start with python -m tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
-    start_cmd = "uvicorn --port 8001 --host 0.0.0.0 runhouse.tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app:app"
-    server_module = rh.server(start_cmd, port=8000).to(cluster, env = ["pytest", "requests"], name="fast_api_module")
+    start_cmd = (
+        "uvicorn --port 8001 --host 0.0.0.0 "
+        "runhouse.tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app:app"
+    )
+    server_module = rh.server(start_cmd, port=8000).to(
+        cluster, env=["pytest", "requests"], name="fast_api_module"
+    )
     assert isinstance(server_module, rh.WebServer)
 
-    res = requests.get("http://localhost:8001/fast_api_module/summer/1", params={"b": 2})
+    res = requests.get(
+        "http://localhost:8001/fast_api_module/summer/1", params={"b": 2}
+    )
     assert res.json() == 3
-    res = requests.post("http://localhost:8001/fast_api_module/my/deeply/hello/nested/endpoint/1", json={"arg3": 2.0})
+    res = requests.post(
+        "http://localhost:8001/fast_api_module/my/deeply/hello/nested/endpoint/1",
+        json={"arg3": 2.0},
+    )
     assert res.json() == ["hello", 1, 2.0]
     res = requests.get("http://localhost:8001/fast_api_module/my/streaming/endpoint")
     assert res.json() == list(range(10))
-    res = requests.get("http://localhost:8001/fast_api_module/my/endpoint/with/optional/body/params/and/header", json={"a": 1, "b": 2}, headers={"c": 3})
+    res = requests.get(
+        "http://localhost:8001/fast_api_module/my/endpoint/with/optional/body/params/and/header",
+        json={"a": 1, "b": 2},
+        headers={"c": 3},
+    )
 
     # Test UI page pulls properly
     res = requests.get("http://localhost:8000/fast_api_module/docs")
@@ -102,6 +151,8 @@ def test_custom_start_cmd(cluster):
     fast_api_module.stop()
     with pytest.raises(requests.exceptions.ConnectionError):
         requests.get("http://localhost:8001/fast_api_module/summer/1", params={"b": 2})
+
+    server_module.stop()
 
 
 if __name__ == "__main__":
