f9ea6dfe0dbf77b3dbd7f738a3919cd6049049a2
dongreenberg <dongreenberg2@gmail.com>
2023-11-17 11:33:55 -0500

    Add factory subclass inference check

--- tests/test_resources/test_modules/test_server_modules/dont_test_server_module.py
+++ tests/test_resources/test_modules/test_server_modules/dont_test_server_module.py
@@ -28,6 +28,7 @@ def test_asgi_server(cluster):
     # We can find the app name by extracting the decorator from the function source:
     # inspect.getsource(app.routes[4].endpoint)
     fast_api_module = rh.server(app).to(cluster, env = ["pytest", "requests"], name="fast_api_module")
+    assert isinstance(fast_api_module, rh.ASGIApp)
     assert fast_api_module.summer(1, 2) == 3
     # Call an async method
     assert asyncio.run(fast_api_module.my_deeply_nested_saync_endpoint("hello", 1, 2.0) == ("hello", 1, 2.0))
@@ -58,6 +59,7 @@ def test_python_script_start(cluster):
     mod = tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
     # Will start with python -m tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
     server_module = rh.server(mod, port=8000).to(cluster, env = ["pytest", "requests"], name="fast_api_module")
+    assert isinstance(mod, rh.WebServer)
 
     res = requests.get("http://localhost:8001/fast_api_module/summer/1", params={"b": 2})
     assert res.json() == 3
@@ -83,6 +85,7 @@ def test_custom_start_cmd(cluster):
     # Will start with python -m tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app
     start_cmd = "uvicorn --port 8001 --host 0.0.0.0 runhouse.tests.test_resources.test_modules.test_server_modules.assets.sample_fastapi_app:app"
     server_module = rh.server(start_cmd, port=8000).to(cluster, env = ["pytest", "requests"], name="fast_api_module")
+    assert isinstance(server_module, rh.WebServer)
 
     res = requests.get("http://localhost:8001/fast_api_module/summer/1", params={"b": 2})
     assert res.json() == 3
