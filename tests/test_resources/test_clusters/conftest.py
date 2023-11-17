d1993e3e8fce782fae5f1001bb7b5b5f32c07870
DenisYay <denis@run.house>
2023-11-17 12:30:41 -0500

    Disable telemetry by default in the local docker public key cluster

--- tests/test_resources/test_clusters/conftest.py
+++ tests/test_resources/test_clusters/conftest.py
@@ -367,7 +367,6 @@ def local_docker_cluster_public_key(request, detached=True):
             "ssh_user": SSH_USER,
             "ssh_private_key": keypath,
         },
-        use_local_telemetry=True,
     )
     c = rh.cluster(**args)
     init_args[id(c)] = args
