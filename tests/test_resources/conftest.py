34d6f4f9ad6fcd4b3b22e36b5802218d330c98f9
Alexandra Belousov <sashabelousovrh@Alexandras-MBP.lan>
2023-11-21 12:06:12 +0200

    added Module unittests

--- tests/test_resources/conftest.py
+++ tests/test_resources/conftest.py
@@ -1,3 +1,4 @@
+import httpx
 import pytest
 
 from runhouse.resources.resource import Resource
@@ -7,6 +8,9 @@ from tests.conftest import init_args
 ######## Constants ########
 
 RESOURCE_NAME = "my_resource"
+
+# Note: API Server will run on local docker container
+BASE_URL = "http://localhost:32300"
 
 ######## Fixtures ########
 
@@ -38,3 +42,12 @@ def local_named_resource():
     r = Resource(**args)
     init_args[id(r)] = args
     return r
+
+
+def http_server_is_up():
+    try:
+        resp = httpx.get(f"{BASE_URL}/check")
+        resp.raise_for_status()  # Will raise an exception for any status code 400 and above
+        return True
+    except httpx.HTTPError:
+        return False
