7fa5d5d4ad61126ea548891093137c5641536e1a
Rohan Sreerama <rohan@run.house>
2023-12-18 20:31:10 -0800

    update(k8s-tests): adds k8s testing class WIP

--- tests/test_resources/test_clusters/test_kubernetes_cluster/test_kubernetes.py
+++ tests/test_resources/test_clusters/test_kubernetes_cluster/test_kubernetes.py
@@ -1,24 +1,62 @@
-import os
-import unittest
+# import os
+# import unittest
+
+# import pytest
+
+# import runhouse as rh
+
+# from tests.test_resources.test_clusters.cluster_tests import np_array, sd_generate_image
+
+# @pytest.mark.clustertest
+# @pytest.mark.k8stest
+# def test_read_shared_k8s_cluster(kubernetes_cpu_cluster):
+#     res = kubernetes_cpu_cluster.run_python(["import numpy", "print(numpy.__version__)"])
+#     assert res[0][1]
+
+# def num_cpus():
+#     import multiprocessing
+#     return f"Num cpus: {multiprocessing.cpu_count()}"
+
+# @pytest.mark.clustertest
+# @pytest.mark.k8stest
+# def test_function_on_k8s_cluster(kubernetes_cpu_cluster):
+#     num_cpus_cluster = rh.function(name="num_cpus_cluster", fn=num_cpus).to(system=kubernetes_cpu_cluster, reqs=["./"])
+#     assert num_cpus_cluster() == 'Num cpus: 4' 
+
+import tests.test_resources.test_clusters.test_cluster
 
 import pytest
 
 import runhouse as rh
 
-from tests.test_resources.test_clusters.cluster_tests import np_array, sd_generate_image
+from tests.conftest import init_args
 
-@pytest.mark.clustertest
-@pytest.mark.k8stest
-def test_read_shared_k8s_cluster(kubernetes_cpu_cluster):
-    res = kubernetes_cpu_cluster.run_python(["import numpy", "print(numpy.__version__)"])
-    assert res[0][1]
+from runhouse.resources.hardware import KubernetesCluster
 
-def num_cpus():
-    import multiprocessing
-    return f"Num cpus: {multiprocessing.cpu_count()}"
+class TestKubernetesCluster(tests.test_resources.test_clusters.test_cluster.TestCluster):
 
-@pytest.mark.clustertest
-@pytest.mark.k8stest
-def test_function_on_k8s_cluster(kubernetes_cpu_cluster):
-    num_cpus_cluster = rh.function(name="num_cpus_cluster", fn=num_cpus).to(system=kubernetes_cpu_cluster, reqs=["./"])
-    assert num_cpus_cluster() == 'Num cpus: 4'
+    MAP_FIXTURES = {"resource": "cluster"}
+
+    UNIT = {"cluster": []}
+    LOCAL = {"cluster": []}
+    MINIMAL = {"cluster": ["kubernetes_cpu_cluster"]}
+
+    @pytest.mark.level("minimal")
+    def test_read_shared_k8s_cluster(kubernetes_cpu_cluster):
+        res = kubernetes_cpu_cluster.run_python(["import numpy", "print(numpy.__version__)"])
+        assert res[0][1]
+
+    # def num_cpus():
+    #     import multiprocessing
+    #     return f"Num cpus: {multiprocessing.cpu_count()}"
+
+    @pytest.mark.level("minimal")
+    def test_function_on_k8s_cluster(kubernetes_cpu_cluster):
+
+        def num_cpus():
+            import multiprocessing
+            return f"Num cpus: {multiprocessing.cpu_count()}"
+        
+        num_cpus_cluster = rh.function(name="num_cpus_cluster", fn=num_cpus).to(system=kubernetes_cpu_cluster, reqs=["./"])
+        assert num_cpus_cluster() == 'Num cpus: 4' 
+    
