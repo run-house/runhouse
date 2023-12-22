4776b2f0139680052fb37909fa586343b63f7f51
DenisYay <denis@run.house>
2023-12-22 14:12:43 -0500

    upgrade the telemetry collector to HTTPS

--- runhouse/rns/defaults.py
+++ runhouse/rns/defaults.py
@@ -31,7 +31,7 @@ class Defaults:
         "use_rns": False,
         "api_server_url": "https://api.run.house",
         "dashboard_url": "https://run.house",
-        "telemetry_collector_address": "api.run.house:14317",
+        "telemetry_collector_address": "https://api.run.house:14318",
     }
 
     def __init__(self):
