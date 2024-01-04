31cef57353fc31050ad7e25bd78282d10083849d
Rohin Bhasin <bhasin.rohin@gmail.com>
2024-01-04 12:32:12 -0500

    Stop tests from hanging locally on conda. (#302)

--- tests/test_resources/test_envs/conftest.py
+++ tests/test_resources/test_envs/conftest.py
@@ -68,7 +68,7 @@ def _local_conda_env():
     os.system(f"conda create -n {env_name} -y python==3.10.9")
     yield
 
-    os.system(f"conda env remove -n {env_name}")
+    os.system(f"conda env remove -n {env_name} -y")
 
 
 @pytest.fixture(scope="function")
