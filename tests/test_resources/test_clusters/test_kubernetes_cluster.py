d0f578797ab0500f069d086eab0f88a4581ba44f
Rohan Sreerama <rohan@run.house>
2024-01-09 09:07:11 -0800

    feat(k8s): adds k8s specific tests

--- tests/test_resources/test_clusters/test_kubernetes_cluster.py
+++ tests/test_resources/test_clusters/test_kubernetes_cluster.py
@@ -4,9 +4,13 @@ import pytest
 
 import runhouse as rh 
 
+def num_cpus():
+    import multiprocessing
+    return f"Num cpus: {multiprocessing.cpu_count()}"
+
 
 @pytest.mark.kubernetestest
-class TestKubernetesCluster(tests.test_resources.test_clusters.test_cluster.TestCluster):
+class TestKubernetesCluster():
 
     MAP_FIXTURES = {"resource": "cluster"}
 
@@ -16,21 +20,12 @@ class TestKubernetesCluster(tests.test_resources.test_clusters.test_cluster.Test
     THOROUGH = {"cluster": []}
     MAXIMAL = {"cluster": []}
 
-    # @pytest.mark.level("minimal")
-    # def test_read_shared_k8s_cluster(kubernetes_cpu_cluster):
-    #     res = kubernetes_cpu_cluster.run_python(["import numpy", "print(numpy.__version__)"])
-    #     assert res[0][1]
+    @pytest.mark.level("minimal")
+    def test_read_shared_k8s_cluster(self, kubernetes_cpu_cluster):
+        res = kubernetes_cpu_cluster.run_python(["import numpy", "print(numpy.__version__)"])
+        assert res[0][1]
 
-    # def num_cpus():
-    #     import multiprocessing
-    #     return f"Num cpus: {multiprocessing.cpu_count()}"
-
-    # @pytest.mark.level("minimal")
-    # def test_function_on_k8s_cluster(kubernetes_cpu_cluster):
-
-    #     def num_cpus():
-    #         import multiprocessing
-    #         return f"Num cpus: {multiprocessing.cpu_count()}"
-
-    #     num_cpus_cluster = rh.function(name="num_cpus_cluster", fn=num_cpus).to(system=kubernetes_cpu_cluster, reqs=["./"])
-    # #     assert num_cpus_cluster() == 'Num cpus: 4'
+    @pytest.mark.level("minimal")
+    def test_function_on_k8s_cluster(self, kubernetes_cpu_cluster):
+        num_cpus_cluster = rh.function(name="num_cpus_cluster", fn=num_cpus).to(system=kubernetes_cpu_cluster, reqs=["./"])
+        assert num_cpus_cluster() == 'Num cpus: 4'  # This test will be improved so that it passes on any cluster setting
