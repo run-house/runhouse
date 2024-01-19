ec073d226eda0b9e364515009da8541e2e98bf74
dongreenberg <dongreenberg2@gmail.com>
2024-01-19 14:33:01 -0500

    Introduce Asgi module and support calling route functions directly
    Introduces Asgi modulei, including logic for incorporating routes as part of signature,
     and asgi factory including logic to find and reload asgi app by pointers.
    Successfully calls route endpoint by method directly. Next needs to support calling via requests/curl.

--- tests/test_resources/test_modules/test_server_modules/dont_test_server_module.py
+++ tests/test_resources/test_modules/test_server_modules/dont_test_server_module.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 import unittest
 
@@ -22,57 +21,47 @@ TODO test with Den Auth and HTTPS enabled
 """
 
 
-def test_asgi_server(cluster):
-    # If we detect that this is a FastAPI app / ASGIApp, we can automatically attach the functions to the  __getattr__
-    # of the app object, so that we can call them directly from the app object. We can also deploy it with uvicorn.
-    # We can find the app name and module via the functions in the app object's routes:
-    # app.routes[4].endpoint.__module__
-    # or maybe app.routes[4].dependant.cache_key[0]
-    # We can find the app name by extracting the decorator from the function source:
-    # inspect.getsource(app.routes[4].endpoint)
-    fast_api_module = rh.server(app).to(
+@pytest.mark.asyncio
+@pytest.mark.level("local")
+async def test_asgi_server(cluster):
+    fast_api_module = rh.asgi(app).to(
         cluster, env=["pytest", "requests"], name="fast_api_module"
     )
-    assert isinstance(fast_api_module, rh.ASGIApp)
+    assert isinstance(fast_api_module, rh.Asgi)
     assert fast_api_module.summer(1, 2) == 3
+
     # Call an async method
-    assert asyncio.run(
-        fast_api_module.my_deeply_nested_saync_endpoint("hello", 1, 2.0)
-        == ("hello", 1, 2.0)
+    assert await fast_api_module.my_deeply_nested_async_endpoint("hello", 1, 2.0) == (
+        "hello",
+        1,
+        2.0,
     )
 
-    assert fast_api_module.my_streaming_endpoint() == list(range(10))
+    assert list(fast_api_module.my_streaming_endpoint()) == list(range(10))
     assert fast_api_module.my_endpoint_with_optional_body_params_and_header(
         a=1, b=2, c=3
     ) == (1, 2, 3)
 
-    res = requests.get(
-        "http://localhost:8000/fast_api_module/summer/1", params={"b": 2}
-    )
+    endpoint = fast_api_module.endpoint(external=False)
+    res = requests.get(f"{endpoint}/summer/1", params={"b": 2})
     assert res.json() == 3
     res = requests.post(
-        "http://localhost:8000/fast_api_module/my/deeply/hello/nested/endpoint/1",
+        f"{endpoint}/my/deeply/hello/nested/endpoint/1",
         json={"arg3": 2.0},
     )
     assert res.json() == ["hello", 1, 2.0]
-    res = requests.get("http://localhost:8000/fast_api_module/my/streaming/endpoint")
+    res = requests.get(f"{endpoint}/my/streaming/endpoint")
     assert res.json() == list(range(10))
     res = requests.get(
-        "http://localhost:8000/fast_api_module/my/endpoint/with/optional/body/params/and/header",
+        f"{endpoint}/my/endpoint/with/optional/body/params/and/header",
         json={"a": 1, "b": 2},
         headers={"c": 3},
     )
     assert res.json() == [1, 2, 3]
 
     # Test UI page pulls properly
-    res = requests.get("http://localhost:8000/fast_api_module/docs")
+    res = requests.get(f"{endpoint}/docs")
     assert res.status_code == 200
-
-    fast_api_module.stop()
-
-    # Test that the server is stopped
-    with pytest.raises(requests.exceptions.ConnectionError):
-        requests.get("http://localhost:8000/fast_api_module/summer/1", params={"b": 2})
 
 
 def test_python_script_start(cluster):
