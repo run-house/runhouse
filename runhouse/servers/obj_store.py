405aaeef78ea9cffdb5ddf5cfb9e72ecaebb15d1
Alexandra Belousov <sashabelousovrh@Alexandras-MacBook-Pro.local>
2024-02-13 15:20:19 +0200

    remove ssh_creds from status return value

--- runhouse/servers/obj_store.py
+++ runhouse/servers/obj_store.py
@@ -1238,8 +1238,7 @@ class ObjStore:
 
     def status(self):
         config_cluster = self.get_cluster_config()
-        if "ssh_creds" in config_cluster.keys():
-            config_cluster.pop("ssh_creds")
+        config_cluster.pop("ssh_creds", None)
         cluster_servlets = {}
         for env in self.get_all_initialized_env_servlet_names():
             resources_in_env_modified = self.call_actor_method(
