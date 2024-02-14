5271da3b81b610bb7c4bb84ac288b7cbfa4d15a0
Alexandra Belousov <sashabelousovrh@Alexandras-MacBook-Pro.local>
2024-02-14 12:01:22 +0200

    remove live_state from cluster

--- tests/test_resources/test_clusters/test_cluster.py
+++ tests/test_resources/test_clusters/test_cluster.py
@@ -196,7 +196,7 @@ class TestCluster(tests.test_resources.test_resource.TestResource):
     def test_rh_status_pythonic(self, cluster):
         cluster.put(key="status_key1", obj="status_value1", env="numpy_env")
         res = cluster.status()
-        assert res.get("ssh_certs") is None
+        assert res.get("creds") is None
         assert res.get("server_port") == (cluster.server_port or DEFAULT_SERVER_PORT)
         assert res.get("server_connection_type") == cluster.server_connection_type
         assert res.get("den_auth") == cluster.den_auth
@@ -220,7 +220,7 @@ class TestCluster(tests.test_resources.test_resource.TestResource):
         assert "Serving 🍦 :" in res
         assert "base_env (runhouse.resources.envs.env.Env):" in res
         assert "status_key2 (str)" in res
-        assert "ssh_certs" not in res
+        assert "creds" not in res
 
     @pytest.mark.skip("Restarting the server mid-test causes some errors, need to fix")
     @pytest.mark.level("local")
