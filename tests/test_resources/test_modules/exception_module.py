a944c1a1506717281fe032548433278d9658f257
Caroline Chen <chen.caroline12@gmail.com>
2024-03-15 15:11:52 -0400

    Raise relevant exceptions in Module's load_config (#595)

---
+++ tests/test_resources/test_modules/exception_module.py
@@ -0,0 +1,10 @@
+import pyarrow  # noqa
+import runhouse as rh
+
+
+class ExceptionModule(rh.Module):
+    def __init__(self):
+        super().__init__()
+
+    def test_fn(self):
+        return None
