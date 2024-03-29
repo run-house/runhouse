9bb6c9b037200fe8c6a4291e24f4c33172c79672
dongreenberg <dongreenberg2@gmail.com>
2024-03-29 13:49:16 -0400

    Tiny performance improvments

--- runhouse/constants.py
+++ runhouse/constants.py
@@ -26,7 +26,7 @@ DEFAULT_RAY_PORT = 6379
 
 DEFAULT_SERVER_HOST = "0.0.0.0"
 
-LOGGING_WAIT_TIME = 1
+LOGGING_WAIT_TIME = 0.5
 
 # Commands
 SERVER_START_CMD = f"{sys.executable} -m runhouse.servers.http.http_server"
