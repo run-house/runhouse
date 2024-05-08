1ded3acfae241b81b3e388f0b983f62982dc54c1
Rohin Bhasin <bhasin.rohin@gmail.com>
2024-05-08 10:28:38 -0400

    Refine parallel embedding example further.

--- examples/parallel-hf-embedding-ec2/parallel_hf_embedding_ec2.py
+++ examples/parallel-hf-embedding-ec2/parallel_hf_embedding_ec2.py
@@ -2,8 +2,7 @@
 
 # This example demonstrates how to use Runhouse primitives to embed a large number of websites in parallel.
 # We use a [BGE large model from Hugging Face](https://huggingface.co/BAAI/bge-large-en-v1.5) and load it via
-# the `HuggingFaceBgeEmbeddings` class from the `langchain` library. We load 4 models onto an instance from AWS EC2
-# that contains 4 A10G GPUs, and can make parallel calls to these models with URLs to embed.
+# the `SentenceTransformer` class from the `huggingface` library.
 #
 # ## Setup credentials and dependencies
 #
@@ -24,15 +23,11 @@
 # $ aws configure
 # $ sky check
 # ```
-# We'll be downloading the model from Hugging Face, so we need to set up our Hugging Face token:
-# ```shell
-# $ export HF_TOKEN=<your huggingface token>
-# ```
 #
 # ## Some utility functions
 #
 # We import `runhouse` and other utility libraries; only the ones that are needed to run the script locally.
-# Imports of libraries that are needed on the remote machine (in this case, the `langchain` dependencies)
+# Imports of libraries that are needed on the remote machine (in this case, the `huggingface` dependencies)
 # can happen within the functions that will be sent to the Runhouse cluster.
 
 import asyncio
@@ -47,7 +42,8 @@ import torch
 from bs4 import BeautifulSoup
 from tqdm.asyncio import tqdm
 
-# Then, we define some utility functions that we'll use for our embedding task.
+# Then, we define an `extract_urls` function that will extract all URLs from a given URL, recursively up to a
+# maximum depth. This'll be a useful helper function that we'll use to collect our list of URLs to embed.
 def _extract_urls_helper(url, visited, original_url, max_depth=1, current_depth=1):
     """
     Extracts all URLs from a given URL, recursively up to a maximum depth.
@@ -97,7 +93,7 @@ def extract_urls(url, max_depth=1):
 #
 # Next, we define a class that will hold the model and the logic to extract a document from a URL and embed it.
 # Later, we'll instantiate this class with `rh.module` and send it to the Runhouse cluster. Then, we can call
-# the functions on this class remotely.
+# the functions on this class and they'll run on the remote machine.
 #
 # Learn more in the [Runhouse docs on functions and modules](/docs/tutorials/api-modules).
 class URLEmbedder:
@@ -126,18 +122,9 @@ class URLEmbedder:
 # ## Setting up Runhouse primitives
 #
 # Now, we define the main function that will run locally when we run this script, and set up
-# our Runhouse module on a remote cluster. First, we create a cluster with the desired instance type and provider.
-# Our `instance_type` here is defined as `A10G:4`, which is the accelerator type and count that we need. We could
-# alternatively specify a specific AWS instance type, such as `p3.2xlarge` or `g4dn.xlarge`. If the cluster was
-# already up (e.g. if we had run this script before), we just bring it up if it's not already up.
-#
-# Learn more in the [Runhouse docs on clusters](/docs/tutorials/api-clusters).
-#
-# :::note{.info title="Note"}
-# Make sure that your code runs within a `if __name__ == "__main__":` block, as shown below. Otherwise,
-# the script code will run when Runhouse attempts to import your code remotely.
-# :::
+# our Runhouse module on a remote cluster.
 async def main():
+
     # We set up some parameters for our embedding task.
     num_replicas = 4  # Number of models to load side by side
     max_concurrency_per_replica = 32  # Number of parallel calls to make to each replica
@@ -148,15 +135,22 @@ async def main():
     urls = extract_urls(url_to_recursively_embed, max_depth=2)
     print(f"Time to extract {len(urls)} URLs: {time.time() - start_time}")
 
-    # Generally, when using Runhouse, you would initialize an env with `rh.env`, and send your module to
-    # that env. Each env runs in a *separate process* on the cluster. In this case, we want to have 4 copies of the
-    # embedding model in separate processes, because we have 4 GPUs. We can do this by creating 4 separate envs
-    # and 4 separate modules, each sent to a separate env. We do this in a loop here, with a list of dependencies
-    # that we need remotely to run the module.
+    # First, we create a cluster with the desired instance type and provider.
+    # Our `instance_type` here is defined as `A10G:1`, which is the accelerator type and count we need We could
+    # alternatively specify a specific AWS instance type, such as `p3.2xlarge` or `g4dn.xlarge`. However, we
+    # provision `num_replicas` number of these instances. This gives us one Runhouse cluster that has
+    # several separate GPU machines that it can access.
     #
-    # Note that we first construct the module locally, then send it to the cluster with `get_or_to`. The instance
-    # returned by `get_or_to` functions exactly the same as a local instance of the module, but when we call a
-    # function (like `initialize_model`) on it, the function is run on the remote machine.
+    # This is one major advantage of Runhouse: you can use a multinode machine as if it were one opaque cluster,
+    # and send things to it from your local machine. This is especially useful for embarrassingly parallel tasks
+    # like this one. Note that it is also far easier to provision several A10G:1 machines as spot instances
+    # than it is to provision a single A10G:4 machine, which is why we do it this way.
+    #
+    # Note that if the cluster was
+    # already up (e.g. if we had run this script before), the code would just bring it up instead of creating a
+    # new one, since we have given it a unique name `"rh-4xa10g"`.
+    #
+    # Learn more in the [Runhouse docs on clusters](/docs/tutorials/api-clusters).
     start_time = time.time()
     embedder_replicas = []
     cluster = rh.cluster(
@@ -165,6 +159,20 @@ async def main():
         num_instances=num_replicas,
         spot=True,
     ).up_if_not()
+
+    # Generally, when using Runhouse, you would initialize an env with `rh.env`, and send your module to
+    # that env. Each env runs in a *separate process* on the cluster. In this case, we want to have 4 copies of the
+    # embedding model in separate processes, because we have 4 GPUs. We can do this by creating 4 separate envs
+    # and 4 separate modules, each sent to a separate env. We do this in a loop here, with a list of dependencies
+    # that we need remotely to run the module.
+    #
+    # In this case, each `env` is also on a separate machine, but we could also provision an A10G:4 instance,
+    # and send all 4 envs to the same machine. Each env runs within a separate process on the machine, so they
+    # won't interfere with each other.
+    #
+    # Note that we send the `URLEmbedder` class to the cluster, and then can construct our modules using the
+    # returned "remote class" instead of the normal local class. These instances are then actually constructed
+    # on the cluster, and any methods called on these instances would run on the cluster.
     for i in range(num_replicas):
         env = rh.env(
             name=f"langchain_embed_env_{i}",
@@ -178,7 +186,7 @@ async def main():
             ],
             compute={"GPU": 1},
         )
-        RemoteURLEmbedder = rh.module(URLEmbedder).to(cluster, env)
+        RemoteURLEmbedder = rh.module(URLEmbedder).get_or_to(cluster, env)
         remote_url_embedder = RemoteURLEmbedder(
             model_name_or_path="BAAI/bge-large-en-v1.5",
             device="cuda",
@@ -189,10 +197,19 @@ async def main():
 
     # ## Calling the Runhouse modules in parallel
     # We'll simply use the `embed_docs` function on the remote module to embed all the URLs in parallel. Note that
-    # we can call this function exactly as if it were a local module. In this case, because we are using the
-    # `asyncio` library to make parallel calls, we need to use a special `run_async=True` argument to the
-    # Runhouse function. This tells Runhouse to return a coroutine that we can await on, rather than making
-    # a blocking network call to the server.
+    # we can call this function exactly as if it were a local module. The semaphore and asyncio logic allows us
+    # to run all the functions in parallel, up to a maximum total concurrency.
+
+    # We pass a few special arguments to the Runhouse function.
+    #
+    # We need to use a special `run_async=True`
+    # argument to the function. This tells Runhouse to return a coroutine that we can await on, rather than making
+    # a blocking network call to the server. This allows us to use `asyncio` logic locally to run all the functions
+    # in parallel.
+    #
+    # We also pass stream_logs=False, which means we won't get the stdout/stderr of the remote
+    # function on our local machine. In this case, we're running a large batch job, and don't want to slow down
+    # our work by spamming our local machine with logs.
     semaphore = asyncio.Semaphore(max_concurrency_per_replica * num_replicas)
 
     async def load_and_embed(url, idx):
@@ -223,5 +240,10 @@ async def main():
     )
 
 
+# :::note{.info title="Note"}
+# Make sure that any code in your Python file that's meant to only run locally runs within
+# a `if __name__ == "__main__":` block, as shown below. Otherwise, the script code will run
+# when Runhouse attempts to import your code remotely.
+# :::
 if __name__ == "__main__":
     asyncio.run(main())
