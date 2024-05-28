f6204afe891ed91e08d10c53ab6e83883c9bbdb5
Rohin Bhasin <bhasin.rohin@gmail.com>
2024-05-28 13:22:32 -0400

    wip embedding mapper

---
+++ examples/embedding_mapper/embedding_mapper_primitive.py
@@ -0,0 +1,131 @@
+import asyncio
+import time
+
+import runhouse as rh
+
+
+def partition_list(lst, num_chunks):
+    chunks = []
+    chunk_size = len(lst) // num_chunks
+    for i in range(num_chunks):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size
+        chunks.append(lst[start:end])
+
+    if len(chunks) > num_chunks:
+        chunks[-2].extend(chunks[-1])
+        chunks = chunks[:-1]
+
+    return chunks
+
+
+class EmbeddingMapper:
+    async def embed_url_recursively(self, url: str, replicas, max_depth: int = 5):
+
+        from langchain_community.document_loaders.recursive_url_loader import (
+            RecursiveUrlLoader,
+        )
+
+        loader = RecursiveUrlLoader(url=url, max_depth=max_depth)
+        print(f"Loading {url} recursively...")
+        docs = list(loader.load())
+        print(f"Loaded {len(docs)} docs.")
+
+        print("Embedding docs...")
+        # Partition into chunks and embed
+        chunked_docs = partition_list(docs, num_chunks=3)
+        parallel_calls = []
+        for i, replica in enumerate(replicas):
+            parallel_calls.append(replica.embed_docs(chunked_docs[i]))
+        await asyncio.gather(*parallel_calls)
+
+
+class DocEmbedder:
+    def __init__(self):
+        self.model = None
+        self.vectorstore = None
+
+    def initialize_model(self):
+        if self.model is None:
+            from langchain.embeddings import HuggingFaceBgeEmbeddings
+
+            model_name = "BAAI/bge-large-en-v1.5"
+            model_kwargs = {"device": "cuda"}
+            encode_kwargs = {
+                "normalize_embeddings": True
+            }  # set True to compute cosine similarity
+
+            self.model = HuggingFaceBgeEmbeddings(
+                model_name=model_name,
+                model_kwargs=model_kwargs,
+                encode_kwargs=encode_kwargs,
+            )
+
+    async def embed_docs(self, docs):
+        from langchain.vectorstores import LanceDB
+        from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+        self.initialize_model()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
+        splits = text_splitter.split_documents(docs)
+
+        # This connects to our existing LanceDB
+
+        # Time embedding
+        start_time = time.time()
+        self.vectorstore = LanceDB(embedding=self.model)
+        self.vectorstore.add_documents(splits)
+        print(f"Time to embed {len(docs)} docs: {time.time() - start_time}")
+
+
+def _get_env_with_name(name):
+    return rh.env(
+        name=name,
+        reqs=[
+            "langchain",
+            "langchain-community",
+            "langchainhub",
+            "lancedb",
+            "bs4",
+            "sentence_transformers",
+        ],
+        secrets=["huggingface"],
+    )
+
+
+async def main():
+    cluster = rh.cluster("rh-a10g", instance_type="A10G:4").save().up_if_not()
+    cluster.restart_server()
+
+    og_env = rh.env(
+        name="langchain_embed_env",
+        reqs=[
+            "langchain",
+            "langchain-community",
+            "langchainhub",
+            "lancedb",
+            "bs4",
+            "sentence_transformers",
+        ],
+        secrets=["huggingface"],
+    )
+    # replicas
+    replicas = []
+    for i in range(3):
+        env = _get_env_with_name(f"langchain_embed_env_{i}")
+        remote_doc_embedder = rh.module(DocEmbedder, name=f"doc_embedder_{i}")().to(
+            system=cluster, env=env
+        )
+        replicas.append(remote_doc_embedder)
+
+    remote_module = rh.module(EmbeddingMapper)().to(system=cluster, env=og_env)
+    await remote_module.embed_url_recursively(
+        "https://js.langchain.com/docs/", replicas
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
