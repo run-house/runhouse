7741865f859f4b19a02f2d1ed6c482026bf95431
Rohin Bhasin <bhasin.rohin@gmail.com>
2024-06-03 21:29:31 -0400

    Send env to a specific `node_idx`. (#835)

--- tests/test_resources/test_clusters/test_multinode_cluster.py
+++ tests/test_resources/test_clusters/test_multinode_cluster.py
@@ -2,6 +2,9 @@ import importlib
 from pathlib import Path
 
 import pytest
+import runhouse as rh
+
+from tests.utils import get_pid_and_ray_node
 
 
 class TestMultiNodeCluster:
@@ -48,3 +51,38 @@ class TestMultiNodeCluster:
         num_nodes = return_value.count(node_marker)
 
         assert num_nodes == 2
+
+    @pytest.mark.level("release")
+    def test_send_envs_to_specific_worker_node(self, multinode_cpu_cluster):
+
+        env_0 = rh.env(
+            name="worker_env_0",
+            reqs=["langchain", "pytest"],
+        ).to(multinode_cpu_cluster, node_idx=0)
+
+        env_1 = rh.env(
+            name="worker_env_1",
+            reqs=["torch", "pytest"],
+        ).to(multinode_cpu_cluster, node_idx=1)
+
+        env_2 = rh.env(
+            name="worker_env_2",
+            reqs=["transformers", "pytest"],
+        )
+
+        with pytest.raises(ValueError):
+            env_2.to(multinode_cpu_cluster, node_idx=len(multinode_cpu_cluster.ips))
+
+        env_2.to(multinode_cpu_cluster, node_idx=1)
+
+        get_pid_0 = rh.function(get_pid_and_ray_node).to(
+            name="get_pid_0", system=multinode_cpu_cluster, env=env_0
+        )
+        get_pid_1 = rh.function(get_pid_and_ray_node).to(
+            name="get_pid_1", system=multinode_cpu_cluster, env=env_1
+        )
+        get_pid_2 = rh.function(get_pid_and_ray_node).to(
+            name="get_pid_2", system=multinode_cpu_cluster, env=env_2
+        )
+        assert get_pid_0()[1] != get_pid_1()[1]
+        assert get_pid_1()[1] == get_pid_2()[1]
