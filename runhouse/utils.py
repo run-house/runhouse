64fde37ecf8b7587fe1f752efcec7a9f5ef5e1ee
Rohin Bhasin <bhasin.rohin@gmail.com>
2024-07-04 10:32:31 -0400

    Move `run_with_logs` into utils. (#965)

--- runhouse/utils.py
+++ runhouse/utils.py
@@ -4,6 +4,7 @@ import functools
 import inspect
 import logging
 import os
+import subprocess
 import sys
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
@@ -12,6 +13,53 @@ from typing import Callable, Type, Union
 import pexpect
 
 logger = logging.getLogger(__name__)
+
+####################################################################################################
+# Simple running utility
+####################################################################################################
+def run_with_logs(cmd: str, **kwargs):
+    """Runs a command and prints the output to sys.stdout.
+    We can't just pipe to sys.stdout, and when in a `call` method
+    we overwrite sys.stdout with a multi-logger to a file and stdout.
+
+    Args:
+        cmd: The command to run.
+        kwargs: Keyword arguments to pass to subprocess.Popen.
+
+    Returns:
+        The returncode of the command.
+    """
+    require_outputs = kwargs.pop("require_outputs", False)
+    stream_logs = kwargs.pop("stream_logs", True)
+
+    p = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        shell=True,
+        **kwargs,
+    )
+
+    out = ""
+    if stream_logs:
+        while True:
+            line = p.stdout.readline()
+            if line == "" and p.poll() is not None:
+                break
+            sys.stdout.write(line)
+            sys.stdout.flush()
+            if require_outputs:
+                out += line
+
+    stdout, stderr = p.communicate()
+
+    if require_outputs:
+        stdout = stdout or out
+        return p.returncode, stdout, stderr
+
+    return p.returncode
+
 
 ####################################################################################################
 # Module discovery and import logic
