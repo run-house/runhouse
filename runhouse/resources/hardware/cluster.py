f27dddb07111fd11e260e27d223a0294ee83320c
Caroline <chen.caroline12@gmail.com>
2024-07-08 17:32:55 -0400

    add basic client checks and setup

--- runhouse/resources/hardware/cluster.py
+++ runhouse/resources/hardware/cluster.py
@@ -199,8 +199,8 @@ class Cluster(Resource):
         # self.on_this_cluster() will still work as expected.
         if on_this_cluster:
             obj_store.set_cluster_config_value("name", self.rns_address)
-        elif self._http_client:
-            self.call_client_method("set_cluster_name", self.rns_address)
+        elif self.client:
+            self.client.set_cluster_name(self.rns_address)
 
         return self
 
@@ -600,11 +600,10 @@ class Cluster(Resource):
 
     # ----------------- RPC Methods ----------------- #
 
-    def call_client_method(self, method_name, *args, **kwargs):
-        method = getattr(self.client, method_name)
-        return method(*args, **kwargs)
+    def connect_server_client(self, force_reconnect=False):
+        if not self.address:
+            raise ValueError(f"No address set for cluster <{self.name}>. Is it up?")
 
-    def connect_tunnel(self, force_reconnect=False):
         if self._rpc_tunnel and force_reconnect:
             self._rpc_tunnel.terminate()
             self._rpc_tunnel = None
