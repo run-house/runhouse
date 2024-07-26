4b5b5f95effd9ab2acd6a42bbf931391e3a5896b
dongreenberg <dongreenberg2@gmail.com>
2024-07-26 12:57:15 -0400

    Add Py312 support

--- setup.py
+++ setup.py
@@ -144,6 +144,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
