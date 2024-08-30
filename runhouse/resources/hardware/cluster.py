56cd67b454847df3d60fdb6eb5451711b09cd3bd
Rohin Bhasin <bhasin.rohin@gmail.com>
2024-08-30 13:48:39 -0400

    Propagate `_ssh_mode`. (#1218)

--- runhouse/resources/hardware/cluster.py
+++ runhouse/resources/hardware/cluster.py
@@ -1428,6 +1428,7 @@ class Cluster(Resource):
                     stream_logs=stream_logs,
                     node=node,
                     require_outputs=require_outputs,
+                    _ssh_mode=_ssh_mode,
                 )
 
                 return return_codes
