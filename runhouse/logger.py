4b300734a4ba3727a0eb187b005fe69ce9ff397f
Donny Greenberg <15992114+dongreenberg@users.noreply.github.com>
2024-09-02 11:27:18 -0400

    Try fixing logs again (#1217)

--- runhouse/logger.py
+++ runhouse/logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 
 
 def get_logger(name) -> logging.Logger:
@@ -16,17 +17,43 @@ def get_logger(name) -> logging.Logger:
         logging.Logger: Configured logger instance.
     """
     # Create or retrieve the logger
-    logger = logging.getLogger(name)
+    return logging.getLogger(name)
 
+
+class NewLineFormatter(logging.Formatter):
+    """Adds logging prefix to newlines to align multi-line messages."""
+
+    def __init__(self, fmt, datefmt=None):
+        logging.Formatter.__init__(self, fmt, datefmt)
+
+    def format(self, record):
+        msg = logging.Formatter.format(self, record)
+        if record.message != "":
+            parts = msg.partition(record.message)
+            msg = msg.replace("\n", "\r\n" + parts[0])
+        return msg
+
+
+root_logger = logging.getLogger("runhouse")
+
+
+def init_logger(logger):
     level = os.getenv("RH_LOG_LEVEL") or "INFO"
     level = getattr(logging, level.upper())
     logger.setLevel(level)
-    formatter = logging.Formatter(
-        "%(levelname)s | %(asctime)s | %(name)s:%(lineno)d | %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
 
-    return logger
+    if not logger.handlers:
+        formatter = NewLineFormatter(
+            "%(levelname)s | %(asctime)s | %(name)s:%(lineno)d | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    logger.propagate = False
+
+
+init_logger(root_logger)
