13ce16b04a7fc1167a69737733308cf447caf7fe
Paul Yang <paulyang@Pauls-MacBook-Pro.local>
2024-09-11 10:56:20 -0400

    remote ##

--- examples/torch-training/airflow-multicloud/local_run_of_callables.py
+++ examples/torch-training/airflow-multicloud/local_run_of_callables.py
@@ -1,11 +1,11 @@
-## You can easily test both the Airflow flow, and the underlying components and code by calling them from local
+# You can easily test both the Airflow flow, and the underlying components and code by calling them from local
 
-## Because the execution has been offloaded to GPU compute on remote, you can call each step from local, or from a notebook
-## You can imagine that a DS or MLE might write a pipeline and interactively debug from local.
-## Then, only when they are confident all the functions work, do they upload the Airflow pipeline which is minimal
+# Because the execution has been offloaded to GPU compute on remote, you can call each step from local, or from a notebook
+# You can imagine that a DS or MLE might write a pipeline and interactively debug from local.
+# Then, only when they are confident all the functions work, do they upload the Airflow pipeline which is minimal
 
-## Airflow is used to schedule, monitor, and retry jobs while providing observability over runs.
-## However, the code that is the substance of the program is not packed into the Airflow DAG.
+# Airflow is used to schedule, monitor, and retry jobs while providing observability over runs.
+# However, the code that is the substance of the program is not packed into the Airflow DAG.
 
 import logging
 
