af2f183e8844e360afd5f2ebd705d66f153ee6ff
dongreenberg <dongreenberg2@gmail.com>
2024-09-30 11:13:24 -0400

    Lint

--- runhouse/resources/distributed/distributed_queue.py
+++ runhouse/resources/distributed/distributed_queue.py
@@ -1,17 +1,22 @@
 import time
 from typing import List, Optional
 
-from runhouse.resources.module import Module
 from runhouse.resources.distributed.supervisor import Supervisor
 from runhouse.resources.functions.function import Function
 
+from runhouse.resources.module import Module
+
 
 class DistributedQueue(Supervisor):
-    def __init__(self, name, replicas: List[Module] = None, max_concurrency = 1, **kwargs):
+    def __init__(
+        self, name, replicas: List[Module] = None, max_concurrency=1, **kwargs
+    ):
         super().__init__(name=name, **kwargs)
         self._replicas = replicas or []
         self._max_concurrency = max_concurrency
-        self._available_replicas = list(range(len(self._replicas) * self._max_concurrency))
+        self._available_replicas = list(
+            range(len(self._replicas) * self._max_concurrency)
+        )
 
     def signature(self, rich=False):
         return self.local._replicas[0].signature(rich=rich)
@@ -22,9 +27,7 @@ class DistributedQueue(Supervisor):
             if timeout == 0:
                 raise TimeoutError("No available replicas.")
             if timeout is not None and time_waited >= timeout:
-                raise TimeoutError(
-                    "Timed out waiting for a replica to be available."
-                )
+                raise TimeoutError("Timed out waiting for a replica to be available.")
             time.sleep(0.25)
             time_waited += 0.25
         worker_idx = self._available_replicas.pop(0)
@@ -38,4 +41,6 @@ class DistributedQueue(Supervisor):
         if isinstance(self._replicas[0], Function):
             return self.call(*args, **kwargs)
         else:
-            raise NotImplementedError("DistributedQueue.__call__ can only be called on Function replicas.")
+            raise NotImplementedError(
+                "DistributedQueue.__call__ can only be called on Function replicas."
+            )
