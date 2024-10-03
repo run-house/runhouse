328b24964f0325fdf838044bb646a18db46066a5
Paul Yang <paulyang@Pauls-MacBook-Pro.local>
2024-10-03 18:08:38 -0400

    update

--- examples/torch-training/kubeflow/kubeflow_torch_train.py
+++ examples/torch-training/kubeflow/kubeflow_torch_train.py
@@ -1,27 +1,23 @@
-import os
-import sys
-
 import kfp
 from kfp import kubernetes
 from kfp.dsl import component, pipeline
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-from TorchBasicExample import download_data, preprocess_data, SimpleTrainer
-
+# from TorchBasicExample import download_data, preprocess_data, SimpleTrainer
 
 # Define the functions for each step in the pipeline
 # We can bring up an on-demand cluster using Runhouse. You can access powerful usage patterns by defining compute in code. All subsequent steps connect to this cluster by name, but you can bring up other clusters for other steps.
-@component(base_image="pypypypy/my-kubeflow-pipeline-image:latest")
+@component(base_image="pypypypy/my-pipeline-image:latest")
 def bring_up_cluster(cluster_name: str, instance_type: str):
 
-    # First we configure the environment to setup Runhouse and AWS credentials
+    # First we configure the environment to setup Runhouse and AWS credentials. We only need to configure the AWS credentials in the first step since the cluster is saved to Runhouse and we reuse the resource.
     import os
 
     import runhouse as rh
 
-    rh.login(token=os.getenv("RUNHOUSE_API_KEY"), interactive=False)
+    rh.login(token=os.getenv("RUNHOUSE_API_KEY"))
 
     import subprocess
 
@@ -46,58 +42,71 @@ def bring_up_cluster(cluster_name: str, instance_type: str):
         check=True,
     )
 
-    # Now we bring up the cluster
+    # Now we bring up the cluster and save it to Runhouse to reuse in subsequent steps. This allows for reuse of the same compute and much better statefulness across multiple Kubeflow steps.
     cluster = rh.ondemand_cluster(
-        name=cluster_name, instance_type=instance_type, provider="aws"
+        name=cluster_name,
+        instance_type=instance_type,
+        provider="aws",
+        autostop_mins=120,
     ).up_if_not()
 
     print(cluster.is_up())
-    cluster.save()  ## Save the cluster by name to Runhouse Den, and I can access that cluster by name later on
+    cluster.save()
 
 
-@component(base_image="pypypypy/my-kubeflow-pipeline-image:latest")
+# This step represents a step to access and lightly preprocess the dataset. The MNIST example is trivial, but we are doing this preprocessing on the same compute we will use later to do the training.
+@component(base_image="pypypypy/my-pipeline-image:latest")
 def access_data(cluster_name: str):
-    # Logging into Runhouse allows me to reuse the cluster I have saved to Runhouse across multiple steps. This can provide flexibility and efficiency when running pipelines.
     import os
+    import sys
 
     import runhouse as rh
 
-    rh.login(token=os.getenv("RUNHOUSE_API_KEY"), interactive=False)
-
-    # Define the requirements on the cluster itself
+    rh.login(token=os.getenv("RUNHOUSE_API_KEY"))
+    # Define the requirements on the remote cluster itself, and send the functions which are on the Docker image to the remote cluster
     env = rh.env(name="test_env", reqs=["torch", "torchvision"])
+    cluster = rh.cluster(
+        name="/paul/" + cluster_name
+    ).up_if_not()  # I am adding /paul/ since I have saved the cluster to Runhouse with my account.
 
-    # I am adding /paul/ since I have saved the cluster to Runhouse with my account.
-    cluster = rh.cluster(name="/paul/" + cluster_name).up_if_not()
+    # Grab the functions from the TorchBasicExample module and send them to the remote cluster
+    sys.path.append(os.path.expanduser("~/training"))
+    from TorchBasicExample import download_data, preprocess_data
+
     remote_download = rh.function(download_data).to(cluster, env=env)
     remote_preprocess = rh.function(preprocess_data).to(cluster, env=env)
 
+    # Run the data download
     remote_download()
-    remote_preprocess()
+    remote_preprocess("./data")
 
 
-@component(base_image="pypypypy/my-kubeflow-pipeline-image:latest")
+# Now we run the training.
+@component(base_image="pypypypy/my-pipeline-image:latest")
 def train_model(cluster_name: str):
     import os
+    import sys
 
     import runhouse as rh
 
-    rh.login(token=os.getenv("RUNHOUSE_API_KEY"), interactive=False)
-
+    rh.login(token=os.getenv("RUNHOUSE_API_KEY"))
     cluster = rh.cluster(name="/paul/" + cluster_name).up_if_not()
-
     env = rh.env(name="test_env", reqs=["torch", "torchvision"])
+
+    # Grab the Trainer class from the TorchBasicExample module and send it to the remote cluster
+    sys.path.append(os.path.expanduser("~/training"))
+    from TorchBasicExample import SimpleTrainer
 
     remote_torch_example = rh.module(SimpleTrainer).to(
         cluster, env=env, name="torch-basic-training"
     )
 
-    model = remote_torch_example()
-
     batch_size = 64
     epochs = 5
     learning_rate = 0.01
 
+    # Instantiate the model (remotely) and run the training loop
+    model = remote_torch_example()
     model.load_train("./data", batch_size)
     model.load_test("./data", batch_size)
 
@@ -110,13 +119,13 @@ def train_model(cluster_name: str):
         )
 
 
-@component(base_image="pypypypy/my-kubeflow-pipeline-image:latest")
+@component(base_image="pypypypy/my-pipeline-image:latest")
 def down_cluster(cluster_name: str):
     import os
 
     import runhouse as rh
 
-    rh.login(token=os.getenv("RUNHOUSE_API_KEY"), interactive=False)
+    rh.login(token=os.getenv("RUNHOUSE_API_KEY"))
 
     cluster = rh.cluster(name="/paul/" + cluster_name)
     cluster.teardown()
@@ -128,22 +137,30 @@ def down_cluster(cluster_name: str):
     description="A simple PyTorch training pipeline with multiple steps",
 )
 def pytorch_training_pipeline(cluster_name: str, instance_type: str):
+    # Define tasks from the components
     bring_up_cluster_task = bring_up_cluster(
         cluster_name=cluster_name, instance_type=instance_type
     )
-
-    env_vars = ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "RUNHOUSE_API_KEY"]
-    secret_name = "my_secret"
-    for var in env_vars:
-        kubernetes.use_secret_as_env(
-            bring_up_cluster_task,
-            secret_name=secret_name,
-            secret_key_to_env={var: var.upper()},
-        )
-
     access_data_task = access_data(cluster_name=cluster_name)
     train_model_task = train_model(cluster_name=cluster_name)
     down_cluster_task = down_cluster(cluster_name=cluster_name)
+
+    # Provide Kubernetes secrets to the tasks
+    env_vars = ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "RUNHOUSE_API_KEY"]
+    tasks = [
+        bring_up_cluster_task,
+        access_data_task,
+        train_model_task,
+        down_cluster_task,
+    ]
+    secret_name = "my-secret"  # This is the name of your secret in Kubernetes secrets
+    for var in env_vars:
+        for task in tasks:
+            kubernetes.use_secret_as_env(
+                task,
+                secret_name=secret_name,
+                secret_key_to_env={var: var.upper()},
+            )
 
     # Define the execution order
     access_data_task.after(bring_up_cluster_task)
