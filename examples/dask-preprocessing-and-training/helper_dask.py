c7991eaf07e92ef43842ddee80759b70a946fb8a
Paul Yang <paulyang@Pauls-MacBook-Pro.local>
2024-11-05 15:35:41 -0500

    dask

---
+++ examples/dask-preprocessing-and-training/helper_dask.py
@@ -0,0 +1,28 @@
+# ## Helper Function: Launch a Dask cluster on the Runhouse cluster
+# We start a scheduler on the head node and workers on the other nodes
+def launch_dask_cluster(cluster):
+    import threading
+
+    def start_scheduler():
+        cluster.run("dask scheduler --port 8786")
+
+    def start_worker(head_node_ip, node):
+        cluster.run(f"dask worker tcp://{head_node_ip}:8786", node=node)
+
+    stable_ips = [ip[0] for ip in cluster.stable_internal_external_ips]
+    head_node_ip = stable_ips[0]
+
+    # Start the scheduler on the head node
+    scheduler_thread = threading.Thread(target=start_scheduler)
+    scheduler_thread.daemon = True
+    scheduler_thread.start()
+
+    # Start workers and connect to the scheduler
+    import time
+
+    for ip in cluster.ips:
+        time.sleep(3)
+        cluster.run('pip install "dask[distributed]"', node=ip)
+        worker_thread = threading.Thread(target=start_worker, args=(head_node_ip, ip))
+        worker_thread.daemon = True
+        worker_thread.start()
---
+++ examples/dask-preprocessing-and-training/launch_lightgbm_dask.py
@@ -0,0 +1,60 @@
+import runhouse as rh
+from helper_dask import launch_dask_cluster
+
+if __name__ == "__main__":
+    # ## Create a Runhouse cluster with 3 nodes
+    num_nodes = 3
+    cluster_name = f"py-{num_nodes}"
+
+    cluster = rh.cluster(
+        name=cluster_name,
+        instance_type="r5d.xlarge",
+        num_instances=num_nodes,
+        provider="aws",
+    ).up_if_not()
+
+    # ## Launch a Dask cluster on the Runhouse cluster
+    # You can do this here if you want the Dask cluster to be ephemeral, or in a separate notebook/script if long lived
+    launch_dask_cluster(cluster)
+
+    from lightgbm_training import LightGBMModelTrainer
+
+    # The environment for the remote cluster
+    env = rh.env(
+        reqs=[
+            "dask-ml",
+            "dask[distributed]",
+            "dask[dataframe]",
+            "boto3",
+            "s3fs",
+            "lightgbm",
+            "cloudpickle",
+        ],
+        secrets=["aws"],
+    )
+
+    # ## Send the trainer class to the remote cluster and instantiate a remote object named 'my_trainer'
+    # You can interact with this trainer class in a different notebook / elsewhere using cluster.get('trainer', remote = True) to get the remote object
+    remote_dask_trainer = rh.module(LightGBMModelTrainer).to(cluster, env=env)
+    dask_trainer = remote_dask_trainer(
+        name="my_trainer"
+    )  # This is a locally callable, but remote instance of the trainer class
+
+    # ## Do the processing and training on the remote cluster
+    # Access the Dask client, data, and preprocess the data
+    dataset_path = "s3://rh-demo-external/taxi/*.parquet"  # 2024 NYC Taxi Data
+    X_vars = ["passenger_count", "trip_distance", "fare_amount"]
+    y_var = "tip_amount"
+
+    dask_trainer.load_client()
+    dask_trainer.load_data(dataset_path)
+    new_date_columns = dask_trainer.preprocess(date_column="tpep_pickup_datetime")
+    X_vars = X_vars + new_date_columns
+    dask_trainer.train_test_split(target_var=y_var, features=X_vars)
+
+    # Train, test, and save the model
+    dask_trainer.train_model()
+    dask_trainer.test_model()
+    dask_trainer.save_model("model.pkl")
+
+    # cluster.teardown() # Optionally, automatically teardown the cluster after training
