c259c9d87f1ced04c3ec0a0bc61cf6417801b89a
Rohin Bhasin <bhasin.rohin@gmail.com>
2024-11-24 19:26:04 -0600

    Remove `disable_data_collection` flag.

--- runhouse/rns/defaults.py
+++ runhouse/rns/defaults.py
@@ -31,7 +31,6 @@ class Defaults:
         "default_autostop": 60,
         "use_spot": False,
         "use_local_configs": True,
-        "disable_data_collection": False,
         "disable_observability": False,
         "use_rns": False,
         "api_server_url": "https://api.run.house",
@@ -295,19 +294,6 @@ class Defaults:
             logger.info(f"Deleted config file from path: {config_path}")
         except OSError:
             raise Exception(f"Failed to delete config file from path {config_path}")
-
-    def disable_data_collection(self):
-        self.set("disable_data_collection", True)
-        os.environ["DISABLE_DATA_COLLECTION"] = "True"
-
-    def data_collection_enabled(self) -> bool:
-        """Checks whether to enable data collection, based on values set in the local ~/.rh config or as an env var."""
-        if self.get("disable_data_collection") is True:
-            return False
-        if os.getenv("DISABLE_DATA_COLLECTION", "False").lower() in ("true", "1"):
-            return False
-
-        return True
 
     @property
     def observability_enabled(self):
