fcf969bba20d1228363acd34bcf282ead96b4427
Alexandra Belousov <sashabelousovrh@Alexandras-MacBook-Pro.local>
2024-11-27 13:02:50 +0200

    include Image in runhouse init

--- runhouse/__init__.py
+++ runhouse/__init__.py
@@ -13,6 +13,8 @@ from runhouse.resources.hardware import (
     OnDemandCluster,
 )
 
+from runhouse.resources.images import Image, ImageSetupStepType
+
 # WARNING: Any built-in module that is imported here must be capitalized followed by all lowercase, or we will
 # will not find the module class when attempting to reconstruct it from a config.
 from runhouse.resources.module import Module, module
