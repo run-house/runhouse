8860ed7f718a8e675eaeb0b83f353c50b6807edd
Paul Yang <paulyang@Pauls-MacBook-Pro.local>
2024-12-04 18:16:05 -0500

    examples

--- examples/dask-preprocessing-and-training/launch_lightgbm_dask_aws.py
+++ examples/dask-preprocessing-and-training/launch_lightgbm_dask_aws.py
@@ -1,10 +1,29 @@
 import runhouse as rh
-from helper_dask import launch_dask_cluster
+from runhouse.resources.images.image import Image
 
 if __name__ == "__main__":
     # ## Create a Runhouse cluster with multiple nodes
     num_nodes = 2
     cluster_name = f"py-new-{num_nodes}"
+
+    img = (
+        Image("dask-env")
+        .install_packages(
+            [
+                "dask[distributed,dataframe]",
+                "dask-ml",
+                "gcsfs",
+                "lightgbm",
+            ],
+        )
+        .set_env_vars(
+            {
+                "OMP_NUM_THREADS": "1",
+                "MKL_NUM_THREADS": "1",
+                "OPENBLAS_NUM_THREADS": "1",
+            }
+        )
+    )
 
     cluster = rh.cluster(
         name=cluster_name,
@@ -12,38 +31,18 @@ if __name__ == "__main__":
         num_nodes=num_nodes,
         provider="aws",
         launcher_type="local",
+        image = img, 
     ).up_if_not()
-
-    # ## Launch a Dask cluster on the Runhouse cluster
-    # You can do this here if you want the Dask cluster to be ephemeral to this training only
-    # or in a separate process if you want it long lived.
-    launch_dask_cluster(cluster)
-
-    # ## Setup the remote training
-    # The environment for the remote cluster
-    env = rh.env(
-        reqs=[
-            "dask-ml",
-            "dask[distributed]",
-            "dask[dataframe]",
-            "boto3",
-            "s3fs",
-            "lightgbm",
-            "cloudpickle",
-        ],
-        secrets=["aws"],
-    )
 
     # ## Send the trainer class to the remote cluster and instantiate a remote object named 'my_trainer'
     # LightGBMModelTrainer is a completely normal class encapsulating training, that a researcher would also be able to use locally as-is
     from lightgbm_training import LightGBMModelTrainer
-
-    remote_dask_trainer = rh.module(LightGBMModelTrainer).to(cluster, env=env)
+    remote_dask_trainer = rh.module(LightGBMModelTrainer).to(cluster)
 
     # Create is a locally callable, but remote instance of the trainer class
     # You can interact with this trainer class in a different notebook / elsewhere using
     # cluster.get('trainer', remote = True) to get the remote object
-    dask_trainer = remote_dask_trainer(name="my_trainer")
+    dask_trainer = remote_dask_trainer(name="my_trainer").distribute("dask")
 
     # ## Do the processing and training on the remote cluster
     # Access the Dask client, data, and preprocess the data
@@ -52,6 +51,7 @@ if __name__ == "__main__":
     y_var = "tip_amount"
 
     dask_trainer.load_client()
+
     dask_trainer.load_data(dataset_path)
     new_date_columns = dask_trainer.preprocess(date_column="tpep_pickup_datetime")
     X_vars = X_vars + new_date_columns
