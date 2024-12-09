555c50d67db98e5a041b337b8518f94b148eda84
Alexandra Belousov <sashabelousovrh@Alexandras-MBP.lan>
2024-12-09 11:06:25 +0200

    update release testing github action

--- tests/fixtures/static_cluster_fixtures.py
+++ tests/fixtures/static_cluster_fixtures.py
@@ -16,18 +16,18 @@ class computeType(str, Enum):
 
 
 def setup_static_cluster(
+    test_rns_folder: str,
     launcher: Union[LauncherType, str] = None,
     compute_type: computeType = computeType.cpu,
 ):
-    from tests.fixtures.resource_fixtures import create_folder_path
-
     rh.constants.SSH_SKY_SECRET_NAME = (
-        f"{create_folder_path()}-{rh.constants.SSH_SKY_SECRET_NAME}"
+        f"{test_rns_folder}-{rh.constants.SSH_SKY_SECRET_NAME}"
     )
     instance_type = "CPU:4" if compute_type == computeType.cpu else "g5.xlarge"
     launcher = launcher if launcher else LauncherType.LOCAL
+    cluster_name = f"{test_rns_folder}-{launcher}-aws-{compute_type}-password"
     cluster = rh.cluster(
-        f"{launcher}-aws-{compute_type}-password",
+        name=cluster_name,
         instance_type=instance_type,
         provider="aws",
         launcher=launcher,
@@ -55,7 +55,13 @@ def setup_static_cluster(
         "ssh_private_key": "~/.ssh/sky-key",
         "password": "cluster-pass",
     }
-    args = dict(name="static-cpu-password", host=[cluster.head_ip], ssh_creds=ssh_creds)
+    # added static to the cluster name so the "under the hood" on demand cluster will not get overridden in den.
+    # This way, when we'll call runhouse cluster down at the end of the nightly release test, both the on-demand cluster
+    # and the static cluster will be terminated. Otherwise, both of the clusters will be terminated using the on-demand
+    # cluster auto-stop.
+    args = dict(
+        name=f"{cluster_name}-static", host=[cluster.head_ip], ssh_creds=ssh_creds
+    )
     c = rh.cluster(**args).save()
     c.restart_server(resync_rh=True)
     init_args[id(c)] = args
@@ -66,26 +72,43 @@ def setup_static_cluster(
 
 
 @pytest.fixture(scope="session")
-def static_cpu_pwd_cluster(request):
-    cluster = setup_static_cluster()
+def static_cpu_pwd_cluster(request, test_rns_folder):
+    cluster = setup_static_cluster(test_rns_folder=test_rns_folder)
     yield cluster
     if not request.config.getoption("--detached"):
-        rh.cluster(name=cluster.name).teardown()
+        # for static cluster fixtures, we first create an ondemand cluster, to mock to user's BYO cluster, spinning it
+        # up and then re-save it in den as a static cluster. We save it with the "static" postfix, so we could bring
+        # down the on-demand cluster using runhouse. The static cluster's status will be updated shortly after the
+        # on demand cluster teardown, by the cluster refresh cronjob, which runs every 3 minutes.
+        on_demand_cluster_name = cluster.name.replace("-static", "")
+        rh.cluster(name=on_demand_cluster_name).teardown()
 
 
 @pytest.fixture(scope="session")
-def static_cpu_pwd_cluster_den_launcher(request):
-    cluster = setup_static_cluster(launcher=LauncherType.DEN)
-    yield cluster
-    if not request.config.getoption("--detached"):
-        rh.cluster(name=cluster.name).teardown()
-
-
-@pytest.fixture(scope="session")
-def static_gpu_pwd_cluster_den_launcher(request):
+def static_cpu_pwd_cluster_den_launcher(test_rns_folder):
     cluster = setup_static_cluster(
-        launcher=LauncherType.DEN, compute_type=computeType.gpu
+        launcher=LauncherType.DEN, test_rns_folder=test_rns_folder
     )
     yield cluster
-    if not request.config.getoption("--detached"):
-        rh.cluster(name=cluster.name).teardown()
+    # for static cluster fixtures, we first create an ondemand cluster, to mock to user's BYO cluster, spinning it
+    # up and then re-save it in den as a static cluster. We save it with the "static" postfix, so we could bring
+    # down the on-demand cluster using runhouse. The static cluster's status will be updated shortly after the
+    # on demand cluster teardown, by the cluster refresh cronjob, which runs every 3 minutes.
+    on_demand_cluster_name = cluster.name.replace("-static", "")
+    rh.cluster(name=on_demand_cluster_name).teardown()
+
+
+@pytest.fixture(scope="session")
+def static_gpu_pwd_cluster_den_launcher(test_rns_folder):
+    cluster = setup_static_cluster(
+        launcher=LauncherType.DEN,
+        compute_type=computeType.gpu,
+        test_rns_folder=test_rns_folder,
+    )
+    yield cluster
+    # for static cluster fixtures, we first create an ondemand cluster, to mock to user's BYO cluster, spinning it
+    # up and then re-save it in den as a static cluster. We save it with the "static" postfix, so we could bring
+    # down the on-demand cluster using runhouse. The static cluster's status will be updated shortly after the
+    # on demand cluster teardown, by the cluster refresh cronjob, which runs every 3 minutes.
+    on_demand_cluster_name = cluster.name.replace("-static", "")
+    rh.cluster(name=on_demand_cluster_name).teardown()
