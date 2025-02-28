efd192bd50d1b04d86d8afad1034a021c19b758b
Caroline <chen.caroline12@gmail.com>
2025-02-28 16:37:29 -0500

    Prioritize synced version over local version

--- runhouse/servers/http/http_client.py
+++ runhouse/servers/http/http_client.py
@@ -849,7 +849,7 @@ class HTTPClient:
         self,
         package: "Package",
         conda_env_name: Optional[str] = None,
-        force_sync_local: bool = False,
+        override_remote_version: bool = False,
     ):
         return self.request_json(
             "/install_package",
@@ -857,7 +857,7 @@ class HTTPClient:
             json_dict=InstallPackageParams(
                 package_config=package.config(),
                 conda_env_name=conda_env_name,
-                force_sync_local=force_sync_local,
+                override_remote_version=override_remote_version,
             ).model_dump(),
         )
 
